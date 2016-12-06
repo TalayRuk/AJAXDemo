44208181212a8f6f364a837a13215d575a05f3ff
rouz majlessi <rmajlessi@gmail.com>
2016-12-05 16:20:04 -0800

    set up inital files for ajax, set up jquery  in index file

---
+++ src/AjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // When Home/Ajax is requested the text bleow is returned. this is the result used in the success function in our javascript.
+        public IActionResult HelloAjax()
+        {
+            return Content("Hello from the controller!", "text/plain");
+        }
+    }
+}
